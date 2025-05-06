8d14e07424a487e72e09ca310c56e4090eef5dd8
Glezel Magsalay <glezeltorres28@gmail.com>
2025-05-06 14:32:58 +0800

    third commit

---
+++ src/TestComponents.jsx
@@ -0,0 +1,11 @@
+const TestComponent = () => {
+    return (
+      <div style={{ padding: "20px", textAlign: "center" }}>
+        <h1>React is working!</h1>
+        <p>If you can see this, React is properly rendering components.</p>
+      </div>
+    )
+  }
+  
+  export default TestComponent
+  
